766a759df47b1cc9fe017dcc8968de1e05220e80
s9rA16Bf4 <14398606+s9rA16Bf4@users.noreply.github.com>
2023-03-11 07:46:18 +0100

    Trying out generics

--- algorithm/bubblesort.go
+++ algorithm/bubblesort.go
@@ -1,1 +1,7 @@
 package algorithm
+
+func Bubblesort[K comparable, V any](m map[K]V) V {
+	var s V
+
+	return s
+}
--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import (
+	"algorithm/bubblesort"
+	"fmt"
+)
+
+func main() {
+	testData1 := []string{"a", "c", "b"}
+
+	fmt.Println(bubblesort.Bubblesort(testData1))
+}
